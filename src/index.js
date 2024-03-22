ed6da5d920d8360c62dd26835d38ac6ec53df9a8
AtarAxia707 <alan910707@gmail.com>
2024-03-22 23:22:40 +0800

    0322 commit

--- src/index.js
+++ src/index.js
@@ -1,10 +1,19 @@
 import  ReactDOM from 'react-dom';
-
+import { HashRouter } from 'react-router-dom';
 import App from './App';
 
+/*
 ReactDOM.render(
   <BrowserRouter basename="/https://ataraxia707.github.io/AxiaWEB/">
     <App /> 
   </BrowserRouter>,
   document.getElementById("root")
+);
+*/
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <HashRouter>
+      <App />
+    </HashRouter>
+  </React.StrictMode>
 );
